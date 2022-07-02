54139cd40da709fad68ed748884e240dc4009691
Joseph Luck <joseph.luck@sky.com>
2022-07-02 13:14:40 +0100

    Fix firebase initialisation

--- app/reactotron.ts
+++
@@ -1,7 +0,0 @@
-import Reactotron from "reactotron-react-native";
-
-export const configure = () => {
-  if (__DEV__) {
-    Reactotron.configure({ port: 9090 }).useReactNative().connect();
-  }
-};
